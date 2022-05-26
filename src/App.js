3954e5451ff33bd343355e5c2ef35f538a43ff97
Anabel <villalobos1998@gmail.com>
2022-05-25 17:13:23 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [students, setStudents] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+    try{
+      const response = await fetch(`https://api.hatchways.io/assessment/students`)
+      if(!response.ok) {
+        throw new Error('Server Error')
+      }
+      let data = await response.json();
+      setStudents(data.students);
+      setError(null)
+    }catch(err) {
+      setError(err.message)
+      setStudents(null)
+    }
+    setIsLoading(true)
+  }
+  getData()
+   }, []);
+   
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading && (
+        <div className="all-students">
+          {students.map((student, i) => {
+          const average = student.grades.reduce((sum, curr) => sum + Number(curr), 0) / student.grades.length
+            return (
+              <div className="student-container" key={i}>
+                <div className="student-image">
+                  <img src={student.pic} />
+                </div>
+                <div className="student-text">
+                  <h1 style={{textTransform: "uppercase"}}>{student.firstName} {student.lastName}</h1>
+                  <div>Email: {student.email}</div>
+                  <div>Company: {student.company}</div>
+                  <div>Skill: {student.skill}</div>
+                  <div>Average: {average}%</div>
+                </div>
+              </div>
+            )
+            })}
+        </div>
+      )}
+    </>
   );
 }
 
